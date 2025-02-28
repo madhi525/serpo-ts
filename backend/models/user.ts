66eb11807f0955e75703c5ff81f581934985b3c1
Muhammad Nurul Adhi <73512187+madhi525@users.noreply.github.com>
2025-03-01 04:13:56 +0700

    Update user.ts
    remove double hashing

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -36,13 +36,5 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true } // Menambahkan createdAt & updatedAt secara otomatis
 );
 
-// 🔒 Hash password sebelum disimpan ke database
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
 
 export default mongoose.model('User', UserSchema);
